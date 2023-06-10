cede98af40cfa32be423fe33d9c8a5ce6dd8f92e
Real_Marshal <remarshal@protonmail.com>
2023-06-11 01:35:08 +0300

    add sfa30 and sgp41

--- README.md
+++ README.md
@@ -4,15 +4,15 @@
 
 Currently connected sensors:
 
-- Sensirion SCD30 for CO2, T and RH
-- Sensirion SPS30 for PM1-10
-
-SFA30 and SGP41 will be added soon.
+- SCD30 for CO2, T and RH
+- SPS30 for PM1-10
+- SFA30 for HCHO (formaldehydes)
+- SGP41 for VOC and NOx (relative values only)
 
 About the circuit:
 
 - 10K pull-up resistors are connected to SPS30's SCL and SDA lines
-- 2 I2C buses are used because I'm not sure if and how a sensor with an internal pull-up resistor (SCD30) can be used with a sensor that requires an external pull-up resistor (SPS30).
+- 2 I2C buses are used because I'm not sure if and how sensors with an internal pull-up resistor can be used with a sensor that requires an external pull-up resistor (SPS30).
 
 | Pin | Usage         |
 | --- | ------------- |
--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,29 @@
 #include <sps30.h>
 #include <TFT_eSPI.h>
 #include <SPI.h>
+#include <SensirionI2CSfa3x.h>
+#include <SensirionI2CSgp41.h>
+#include <VOCGasIndexAlgorithm.h>
+#include <NOxGasIndexAlgorithm.h>
 
 // TODO: refactor this awful code & add color ranges for values
+
+#define SPS30_AUTO_CLEAN_DAYS 4
+// Never increase this value unless you want to burn the sensor
+#define SGP41_CONDITIONING_SECS 10
+#define SGP41_SAMPLING_INTERVAL 0.1f
 
 TFT_eSPI tft = TFT_eSPI();
 
 SensirionI2cScd30 scd30;
+SensirionI2CSfa3x sfa3x;
+SensirionI2CSgp41 sgp41;
 
-static char errorMessage[128];
+VOCGasIndexAlgorithm voc_algorithm(SGP41_SAMPLING_INTERVAL);
+NOxGasIndexAlgorithm nox_algorithm;
+
+static char errorMessage[256];
 static int16_t error;
-
-uint8_t auto_clean_days = 4;
-uint32_t auto_clean;
 
 void setup()
 {
@@ -25,7 +36,7 @@ void setup()
 
   Serial.begin(115200);
 
-  // This bus is used for SCD30, 18 SDA, 17 SCL (default I2C pins)
+  // This bus is used for SCD30, SFA30 and SGP41, 18 SDA, 17 SCL (default I2C pins)
   Wire.begin();
   scd30.begin(Wire, SCD30_I2C_ADDR_61);
 
@@ -61,6 +72,32 @@ void setup()
     return;
   }
 
+  sfa3x.begin(Wire);
+
+  error = sfa3x.startContinuousMeasurement();
+  if (error)
+  {
+    Serial.print("Error trying to execute startContinuousMeasurement(): ");
+    errorToString(error, errorMessage, 256);
+    Serial.println(errorMessage);
+  }
+
+  sgp41.begin(Wire);
+
+  uint16_t testResult;
+  error = sgp41.executeSelfTest(testResult);
+  if (error)
+  {
+    Serial.print("Error trying to execute executeSelfTest(): ");
+    errorToString(error, errorMessage, 256);
+    Serial.println(errorMessage);
+  }
+  else if (testResult != 0xD400)
+  {
+    Serial.print("executeSelfTest failed with error: ");
+    Serial.println(testResult);
+  }
+
   // This bus is used for SPS30, 21 SDA, 16 SCL
   Wire1.begin(21, 16);
 
@@ -70,7 +107,7 @@ void setup()
     delay(500);
   }
 
-  error = sps30_set_fan_auto_cleaning_interval_days(auto_clean_days);
+  error = sps30_set_fan_auto_cleaning_interval_days(SPS30_AUTO_CLEAN_DAYS);
   if (error)
   {
     Serial.print("SPS30 error setting the auto-clean interval: ");
@@ -88,11 +125,29 @@ float co2Concentration = 0.0;
 float temperature = 0.0;
 float humidity = 0.0;
 
+int16_t hcho;
+int16_t humidity_sfa;
+int16_t temperature_sfa;
+
+uint16_t defaultRh = 0x8000;
+uint16_t defaultT = 0x6666;
+uint16_t srawVoc = 0;
+uint16_t srawNox = 0;
+int32_t voc_index = 0;
+int32_t nox_index = 0;
+
 struct sps30_measurement sps30_m;
+
+ulong initMillis = 0;
 
 void loop()
 {
   uint16_t dataReady = 0;
+
+  if (initMillis == 0)
+  {
+    initMillis = millis();
+  }
 
   // SCD30
   error = scd30.getDataReady(dataReady);
@@ -130,6 +185,40 @@ void loop()
     }
   }
 
+  // SFA30
+  error = sfa3x.readMeasuredValues(hcho, humidity_sfa, temperature_sfa);
+  if (error)
+  {
+    Serial.print("SFA30 reading measurement data error: ");
+    errorToString(error, errorMessage, 256);
+    Serial.println(errorMessage);
+  }
+
+  // SGP41
+  defaultRh = uint16_t(humidity) * 65535 / 100;
+  defaultT = (uint16_t(temperature) + 45) * 65535 / 175;
+
+  if (millis() - initMillis < SGP41_CONDITIONING_SECS * 1000)
+  {
+    error = sgp41.executeConditioning(defaultRh, defaultT, srawVoc);
+  }
+  else
+  {
+    error = sgp41.measureRawSignals(defaultRh, defaultT, srawVoc, srawNox);
+  }
+
+  if (error)
+  {
+    Serial.print("SGP41 error: ");
+    errorToString(error, errorMessage, 256);
+    Serial.println(errorMessage);
+  }
+  else
+  {
+    voc_index = voc_algorithm.process(srawVoc);
+    nox_index = nox_algorithm.process(srawNox);
+  }
+
   tft.setCursor(0, 0, 2);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
   tft.setTextSize(1);
@@ -150,10 +239,26 @@ void loop()
   tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
   tft.println("%");
 
+  tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
+  tft.print("HCHO: ");
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.print(hcho / 5.0f);
+  tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
+  tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
+  tft.print("ppb   VOC: ");
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.print(voc_index);
+  tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
+  tft.print("u   NOx: ");
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.print(nox_index);
+  tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
+  tft.println("u");
+
   tft.println();
 
   tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
-  tft.print("                        NC 0.5:  ");
+  tft.print("                          NC 0.5:  ");
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
   tft.print(sps30_m.nc_0p5);
   tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
@@ -201,7 +306,7 @@ void loop()
   tft.print(sps30_m.mc_10p0);
   tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
   tft.print("mcg/m3");
-  tft.print(" NC 10.0: ");
+  tft.print("  NC 10.0: ");
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
   tft.print(sps30_m.nc_10p0);
   tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
